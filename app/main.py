b84460730422d64dab06e08bad6297561bfcc85c
Frankeloff <ruskiborg@list.ru>
2022-08-29 21:04:51 +0300

    delete comments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-import uvicorn
 from fastapi import FastAPI
-from sqlalchemy import create_engine
 
 from app.database.database import engine
 from app.models import metadata
@@ -12,11 +10,7 @@ app = FastAPI(title="Employment exchange")
 
 @app.on_event("startup")
 async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(metadata.create_all)
+    metadata.bind = engine
 
 
 app.include_router(api_router, prefix="/api")
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
